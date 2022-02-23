c28ca9fd918eaddc596dd038e91117b5b0e2c637
xiaoraocoding <3490755245@qq.com>
2022-02-23 15:17:50 +0800

    工具类函数

--- helpers/byte.go
+++ helpers/byte.go
@@ -1,9 +1,13 @@
 package helpers
 
+import "strconv"
+
 func Copy(src []byte) []byte {
 	dst := make([]byte, len(src))
 	copy(dst, src)
 	return dst
 }
 
-
+func JoinAddressAndPort(address string, port int) string {
+	return address + ":" + strconv.Itoa(port)
+}
